92f0275001ad4ac15cf56308b93961871302c8ea
Glommydemin <52195266+Glommydemin@users.noreply.github.com>
2019-06-29 07:32:01 +0700

    откат

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 $('.owl-carousel').owlCarousel({
+    items:1,
     loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    margin:20,
+    autoHeight:false
+
 })
